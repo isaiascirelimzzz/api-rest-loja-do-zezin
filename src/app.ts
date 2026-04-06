6b151cc1fb29350c2730a5e19fbb00790708bb85
Isaias Cireli Mezzari <isaiascirelimezzari18@gmail.com>
2026-04-06 17:05:56 -0300

    server configurado

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import route from './routers/user.router.js';
+import { errorHandler } from './middlewares/errorHandler.middleware.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.use()
+app.use("/users", route);
+
+app.use(errorHandler)
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log(`Server was started at port : 3000`)
+})
